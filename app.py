92b4f01fbf8cf77535649e748fac702efdfd13b8
sonydotgit <forhoooman@gmail.com>
2022-08-26 11:25:46 +0530

    slice project id

--- app.py
+++ app.py
@@ -109,6 +109,8 @@ def pdetails():
               (%s, %s, %s, %s)
               """
 
+    user_dict['project_id'] = user_dict['project_id'][:10]
+
     try:
         cur.execute(p_ins_q, (user_dict['project_id'],
                               user_dict['client_id'],
